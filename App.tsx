c515b0b0bf1a3a2ab641088187b8ec80b2bcee5f
conorhutchins <conorhutchins@btinternet.com>
2023-07-27 16:57:04 +0100

    adds calculatewinner func to App.tsx

--- App.tsx
+++ App.tsx
@@ -1,5 +1,26 @@
-import React, {useState} from 'react';
-import {Board} from './src/components/Board';
+import React, { useState } from 'react';
+import { Text, View } from 'react-native';
+import { Board } from './Board';
+
+function calculateWinner(squares: Array<'X' | 'O' | null>): 'X' | 'O' | null {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
 
 const App: React.FC = () => {
   const [squares, setSquares] = useState<Array<'X' | 'O' | null>>(
@@ -7,18 +28,31 @@ const App: React.FC = () => {
   );
   const [xIsNext, setXIsNext] = useState<boolean>(true);
 
-const handlePress = (index: number) => {
-    if (squares[index] != null) {
-      // if square is already filled, do nothing
+  const winner = calculateWinner(squares);
+  let status;
+  if (winner) {
+    status = 'Winner: ' + winner;
+  } else {
+    status = 'Next player: ' + (xIsNext ? 'X' : 'O');
+  }
+
+  const handlePress = (index: number) => {
+    if (squares[index] != null || winner != null) {
+      // Square is already filled or game is over, do nothing
       return;
     }
     const newSquares = squares.slice();
     newSquares[index] = xIsNext ? 'X' : 'O';
     setSquares(newSquares);
-    setXIsNext(!xIsNext); // Switch turns
+    setXIsNext(!xIsNext);  // Switch turns
   };
 
-  return <Board squares={squares} onPress={handlePress} />;
+  return (
+    <View>
+      <Text>{status}</Text>
+      <Board squares={squares} onPress={handlePress} />
+    </View>
+  );
 };
 
 export default App;
