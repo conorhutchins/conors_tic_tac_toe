882161aef065b156ffef0d15cc3b9aa501899cca
conorhutchins <conorhutchins@btinternet.com>
2023-08-02 19:22:23 +0100

    handles winner and turn management in App.tsx

--- App.tsx
+++ App.tsx
@@ -2,9 +2,7 @@ import React, {useState} from 'react';
 import {Text, View, StyleSheet, Modal, TouchableOpacity} from 'react-native';
 import {Board} from './src/components/Board';
 
-// Function to calculate the winner
 function calculateWinner(squares: Array<'X' | 'O' | null>): 'X' | 'O' | null {
-  // These are the 8 possible winning combinations
   const lines = [
     [0, 1, 2],
     [3, 4, 5],
@@ -15,63 +13,50 @@ function calculateWinner(squares: Array<'X' | 'O' | null>): 'X' | 'O' | null {
     [0, 4, 8],
     [2, 4, 6],
   ];
-  // Check each combination to see if it's a winning one
   for (let i = 0; i < lines.length; i++) {
     const [a, b, c] = lines[i];
-    // If all squares in a combination are filled with the same player's mark, return the winner
     if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
       return squares[a];
     }
   }
-  // If no winner, return null
   return null;
 }
 
-// The main component of the app
 const App: React.FC = () => {
-  // The state of the game board and whose turn it is
   const [squares, setSquares] = useState<Array<'X' | 'O' | null>>(
     Array(9).fill(null),
   );
   const [xIsNext, setXIsNext] = useState<boolean>(true);
   const [modalVisible, setModalVisible] = useState<boolean>(false);
 
-  // Determine the winner
   const winner = calculateWinner(squares);
-  // Determine the game status
   let status;
   if (winner) {
     status = 'The winner is ' + winner + '! ';
   } else {
-    status = "No winner! " + (xIsNext ? 'X' : 'O')+"'s" + " goes first next time";
+    status = "No winner! " + (xIsNext ? 'O' : 'X')+"'s" + " goes first next time";
   }
 
-  // Handle square presses
   const handlePress = (index: number) => {
-    // If the square is already filled or the game is over, do nothing
     if (squares[index] != null || winner != null) {
       return;
     }
-    // Otherwise, fill the square with the current player's mark, update the state, and switch turns
     const newSquares = squares.slice();
     newSquares[index] = xIsNext ? 'X' : 'O';
-    const localWinner = calculateWinner(newSquares); // Calculate the winner here
+    const localWinner = calculateWinner(newSquares); 
     setSquares(newSquares);
     setXIsNext(!xIsNext);
-    // If the game is over, show the modal
     if (localWinner || !newSquares.includes(null)) {
       setModalVisible(true);
     }
   };
 
-  // function to reset the game
   const handleRestartFromModal = () => {
     setSquares(Array(9).fill(null));
-    setXIsNext(true);
+    setXIsNext(!xIsNext);
     setModalVisible(false);
   };
 
-  // Render the game board and status message
   return (
     <View style={styles.container}>
       <Modal
