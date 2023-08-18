71f6aba70064a82c9afe8cd99ce403567c211cc0
conorhutchins <conorhutchins@btinternet.com>
2023-08-18 14:47:20 +0100

    adds mock testing for on press events

--- __tests__/Board.test.tsx
+++ __tests__/Board.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {render} from '@testing-library/react-native';
+import {render, fireEvent} from '@testing-library/react-native';
 import {Board} from '../src/components/Board';
 
 describe('Board component', () => {
@@ -7,5 +7,15 @@ it('initially renders correctly', () => {
         const squares = Array(9).fill(null);
         const { getByTestId } = render(<Board squares={squares} onPress={() => {}} />);
         expect(getByTestId('board')).toBeTruthy();
-      });
+});
+it('calls onPress with the correct index when a square is pressed', () => {
+    const onPressMock = jest.fn();  // this is the mock function
+    const squares = Array(9).fill(null); 
+    const { getAllByTestId } = render(<Board squares={squares} onPress={onPressMock} />);
+  
+    fireEvent.press(getAllByTestId('square')[0]); // simulates a press on the first square
+  
+    expect(onPressMock).toHaveBeenCalledWith(0);
+  });
+
     });
