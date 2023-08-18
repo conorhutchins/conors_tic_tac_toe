797b024e8f8d35ab3ccf86ffd34dac4975791fe1
conorhutchins <conorhutchins@btinternet.com>
2023-08-18 14:21:31 +0100

    adds in jest config for testing

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,19 @@
 module.exports = {
   preset: 'react-native',
+  setupFilesAfterEnv: [
+    '@testing-library/jest-native/extend-expect',
+  ],
+  transformIgnorePatterns: [
+    'node_modules/(?!(jest-)?react-native|@react-native|@react-navigation)',
+  ],
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+  },
+  globals: {
+    'ts-jest': {
+      babelConfig: true,
+    },
+  },
+  testMatch: ['**/__tests__/**/*.ts?(x)', '**/?(*.)+(spec|test).ts?(x)'],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
 };
