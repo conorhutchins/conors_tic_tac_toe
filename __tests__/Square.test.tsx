fc71d3b8ba66b1c4e72215e1bc737fe6819e4c86
conorhutchins <conorhutchins@btinternet.com>
2023-08-18 14:55:18 +0100

    adds jest testing to the Square component

---
+++ __tests__/Square.test.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import {render, fireEvent} from '@testing-library/react-native';
+import {Square} from '../src/components/Square';
+
+describe('Square component', () => {
+  it('renders correctly with given value', () => {
+    const {getByText} = render(<Square value="❌" onPress={() => {}} />);
+    expect(getByText('❌')).toBeTruthy();
+  });
+
+  it('calls onPress when pressed', () => {
+    const onPressMock = jest.fn();
+    const { getByTestId } = render(<Square value="❌" onPress={onPressMock} />);
+    fireEvent.press(getByTestId('square'));
+    expect(onPressMock).toHaveBeenCalled();
+  });
+});
