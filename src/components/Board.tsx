8d7604da11bbd93db4e51f49516d08718a8f2790
conorhutchins <conorhutchins@btinternet.com>
2023-10-02 16:18:19 +0100

    connects components to redux

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,17 +1,32 @@
 import React from 'react';
-import {View, StyleSheet} from 'react-native';
-import {Square} from './Square';
+import { View, StyleSheet } from 'react-native';
+import { useSelector, useDispatch } from 'react-redux';
+import { Square } from './Square';
+import { makeMove } from '../redux/game/gameSlice' // imports the action makeMove
+import { calculateWinner } from '../../App';
 
 type BoardProps = {
-  squares: ('❌' | '⭕️' | null)[];
-  onPress: (index: number) => void;
+
 };
 
-export const Board: React.FC<BoardProps> = ({squares, onPress}) => {
-  return (
+export const Board: React.FC<BoardProps> = () => {
+  const squares = useSelector((state) => state.game.squares) // access the game state
+  const dispatch = useDispatch() // get the dispatch function
+
+  // define a func to handle square presses and dispatch the makeMove action
+  const handleSquarePress = (index: number) => {
+    // check if the square is already filled of if theres a winner
+    if (squares[index] || calculateWinner(squares)) {
+      return;
+    }
+    // dispatch the makeMove action with selected square index
+    dispatch(makeMove(index));
+  };
+
+return (
     <View style={styles.board} testID="board">
       {squares.map((square, i) => (
-        <Square key={i} value={square} onPress={() => onPress(i)} />
+        <Square key={i} value={square} onPress={() => handleSquarePress(i)} />
       ))}
     </View>
   );
